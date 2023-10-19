b970182be7ea099ee7736741f3ab5a40382490e3
MK-kapahi <mkapahi84@gmail.com>
2023-10-19 20:24:12 +0530

    --added functinality

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,19 @@
 document.getElementById("Ham-icon").addEventListener("click" ,()=>{
     console.log("hello");
 })
+
+
+document.getElementById("iHme").addEventListener("click" ,()=>{
+    document.getElementById("info_hme").style.display="block"
+    document.getElementById("info_hme").style.color="black"
+})
+
+document.getElementById("ipharma").addEventListener("click" ,()=>{
+    document.getElementById("info_pharma").style.display="block";
+    document.getElementById("info_pharma").style.color="black"
+})
+
+document.getElementById("iO&P").addEventListener("click" ,()=>{
+    document.getElementById("info_OP").style.display="block"
+    document.getElementById("info_OP").style.color="black"
+})
